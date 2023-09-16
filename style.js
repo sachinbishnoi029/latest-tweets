792270c93ec50de8f573a75dece9fedb3fe944d2
sachinbishnoi02 <sb9350680639@gmail.com>
2023-09-16 12:04:06 +0530

    commit

---
+++ style.js
@@ -0,0 +1,39 @@
+$('.slick').slick({
+    dots: false,
+    infinite: true,
+    autoplay: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: '.btn-prev',
+    nextArrow: '.btn-next',
+
+    responsive: [{
+        breakpoint: 1100,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false,
+        }
+    },
+    {
+        breakpoint: 840,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            speed: 1000,
+        }
+    },
+    {
+        breakpoint: 480,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+
+        }
+    }
+    ]
+});
